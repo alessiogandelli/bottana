629ae719c6784bfbfc2e7008327dec62ea5b0174
alessiogandelli <alessiogandelli99@gmail.com>
2022-02-12 21:20:26 +0100

    scegliere le lingue

--- bot.js
+++ bot.js
@@ -1,34 +1,93 @@
 const { Telegraf } = require('telegraf')
 const translate = require("deepl");
-const  dotenv = require('dotenv');
+const database = require('./database');
+const dotenv = require('dotenv');
+const { getCollection } = require('./database');
 dotenv.config()
-
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start((ctx) => ctx.reply('scrivi qualcosa e traduco in francese'))
-bot.on('message',(ctx) => traduci(ctx))
-bot.launch()
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
-function traduci(ctx){
+async function main(){
+
+    await startdb();
+    await new Promise(r => setTimeout(r, 2000));
+    database.getCollection('users').createIndex({username:1}, {unique:true})
+
+    bot.start((ctx) => startbot(ctx))
+    bot.command('/francese', (ctx) => setLanguage(ctx, "FR"))
+    bot.command('/spagnolo', (ctx) => setLanguage(ctx, "ES"))
+    bot.command('/inglese', (ctx) => setLanguage(ctx, "EN-GB"))
+    bot.command('/cinese', (ctx) => setLanguage(ctx, "ZH"))
+    bot.command('/giapponese', (ctx) => setLanguage(ctx, "JA"))
+    bot.command('/portoghese', (ctx) => setLanguage(ctx, "PT-BR"))
+    bot.command('/russo', (ctx) => setLanguage(ctx, "RU"))
+    bot.command('/tedesco', (ctx) => setLanguage(ctx, "DE"))
+
+
+    bot.on('message',(ctx) => traduci(ctx))
+    bot.launch()
+    
+    // Enable graceful stop
+    process.once('SIGINT', () => bot.stop('SIGINT'))
+    process.once('SIGTERM', () => bot.stop('SIGTERM'))
+}
+
+
+
+function startbot(ctx){
+    ctx.reply('che lingua vuoi tradurre? /francese /inglese /cinese /giapponese /portoghese /russo /tedesco /spagnolo') 
+
+
+}
+
+
+async function startdb(){
+    await database.connect()
+}
+
+
+async function setLanguage(ctx, lang){
+    ctx.reply('hai scelto '+ lang)
+
+    try {
+        await database.getCollection('users').updateOne({'username': ctx.message.from.username},{$set:{'lingua': lang}}, {upsert: true})
+
+    } catch (error) {
+        ctx.reply('probabilmente sei gia registrato')
+    }
+}
+
+
+
+
+async function traduci(ctx){
     let id = ctx.message.from.id
 
-    translate({
-        free_api: true,
-        text: ctx.message.text,
-        target_lang: 'FR',
-        auth_key: process.env.DEEPL_TOKEN,
-      })
-      .then(result => {
-         // ctx.reply(result.data.translations[0].text);
-         bot.telegram.sendMessage(id, ctx.message.text +'\n\n' + result.data.translations[0].text)
-      })
-      .catch(error => {
-          console.error(error)
-      });
+    lingua = (await database.getCollection('users').findOne({'username':ctx.message.from.username}))?.lingua
+
+    if(!lingua){
+        ctx.reply('non hai selezionato una lingua')
+    }else{
+        
+        translate({
+            free_api: true,
+            text: ctx.message.text,
+            target_lang: lingua,
+            auth_key: process.env.DEEPL_TOKEN,
+          })
+          .then(result => {
+             // ctx.reply(result.data.translations[0].text);
+             bot.telegram.sendMessage(id, ctx.message.text +'\n\n' + result.data.translations[0].text)
+          })
+          .catch(error => {
+              console.error(error)
+          });
+    }
+
     
 }
+
+
+main()
--- database.js
+++ database.js
@@ -1,7 +1,46 @@
 var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://gandi:diocane@traduttore.zveua.mongodb.net/gandi?retryWrites=true&w=majority";
 
-MongoClient.connect(process.env.DB_URL, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
+let connection;
+
+async function connect(){
+  connection = await (new MongoClient(process.env.CONNECTION_STRING , { useUnifiedTopology: true }).connect());
+
+  return connection
+}
+
+function getConnection() {
+  if (connection === null) {
+      throw new Error('Connection not found');
+  }
+  return connection;
+}
+
+function closeConnection() {
+  connection?.close();
+  connection = null;
+}
+
+function getDb(){
+  if (connection === null) {
+      throw new Error('Connection not found');
+  }
+  return connection?.db(process.env.DEFAULT_DB);
+}
+
+function getCollection(collection){
+  return getDb().collection(collection);
+}
+
+function cleanCollection(collection){
+  getCollection(collection).remove({}) // deprecato 
+}
+
+
+module.exports = {
+  connect: connect,
+  getConnection: getConnection,
+  closeConnection: closeConnection,
+  getCollection: getCollection,
+  cleanCollection: cleanCollection
+};
